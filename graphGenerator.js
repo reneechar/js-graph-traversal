ba59eecb4e4f61d53a8299e0a0c184a2aecd8a6e
Renee <rchar20@gmail.com>
2016-11-15 11:37:01 -1000

    adds properties and addNeighbor function onto Node

--- graphGenerator.js
+++ graphGenerator.js
@@ -1,7 +1,15 @@
 // Implement a graph generator that represents a set of objects where some pairs of objects are connected by links.
 
 const Node = function(name, value) {
+  this.name = name;
+  this.value = value;
+  this.neighbors = [];
 
+  this.addNeighbors = (arr) => {
+    arr.forEach(neighbor => {
+      this.neighbors.push(neighbor);
+    })
+  }
 }
 
 
