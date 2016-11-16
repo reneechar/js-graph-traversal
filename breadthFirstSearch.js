6cd2a7408f98095e154cb43256d02464c67c19cf
Renee <rchar20@gmail.com>
2016-11-15 15:46:05 -1000

    breadth first search passing all tests

--- breadthFirstSearch.js
+++ breadthFirstSearch.js
@@ -4,6 +4,21 @@ const Node = require('./graphGenerator');
 
 const BFS = (start) => {
 
+  let list = [start.name];
+  if (start.neighbors.length === 0) {
+    return list;
+  }
+
+  start.neighbors.forEach(neighbor => {
+    list.push(neighbor.name);
+  })
+  start.neighbors.forEach(neighbor => {
+    let next = BFS(neighbor).slice(1);
+    list = list.concat(next)
+  })
+
+
+  return list
 }
 
 module.exports = BFS;
